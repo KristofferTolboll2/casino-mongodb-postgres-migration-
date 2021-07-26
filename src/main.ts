4be5600494875c986066c011f937a9534adc3a9e
abc <abc@example.com>
2021-07-26 16:53:31 +0200

    added configuration

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,19 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { configureEnvironment } from './enviroment';
+import { Enviroment } from './enviroment'
 
 const apiDescription = `
 This API is used to expose all data, that is related to <a href="https://gurucasinobonus.com>GuruCasinoBonus</a>`;
 
+
+export const ENV = Object.freeze(configureEnvironment(process.env.NODE_ENV));
+export const isDevlopment = ENV === Enviroment.DEVELOPMENT;
 const logger = new Logger('main.ts');
+
 async function bootstrap() {
+  console.log(isDevlopment)
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService>(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
